eb03a3827fd0398b790507a7623f97d02e26b9c1
windseat <windseat@naver.com>
2021-10-20 16:32:10 +0900

    2021 10 20 스터디 업데이트(사무실)

--- lib/04_widget_01.dart
+++ lib/04_widget_01.dart
@@ -17,6 +17,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final items = List.generate(100, (i) => i).toList();
+
 class Widget01 extends StatelessWidget {
   const Widget01({Key? key}) : super(key: key);
 
@@ -27,13 +29,14 @@ class Widget01 extends StatelessWidget {
         title: Text('Flutter Widget 01'),
       ),
       body: PageView(
+        // 여러페이지를 좌우로 슬라이드하여 넘길 수 있도록 해 주는 위젯
         children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Text(
-                'Container Widget',
+                'Container Widget in Column',
                 style: TextStyle(fontSize: 24),
               ),
               Container(
@@ -47,43 +50,126 @@ class Widget01 extends StatelessWidget {
               Container(
                 child: Text('Container'),
                 color: Colors.red,
-                width: 100,
-                height: 100,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Container(
-                child: Text('Container'),
-                color: Colors.red,
-                width: 100,
+                width: 200,
                 height: 100,
                 padding: const EdgeInsets.all(8.0),
                 margin: const EdgeInsets.all(8.0),
               ),
               Container(
-                child: Text('Container'),
-                color: Colors.green,
-                width: 100,
-                height: 100,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-              Container(
-                child: Text('Container'),
                 color: Colors.blue,
                 width: 100,
                 height: 100,
                 padding: const EdgeInsets.all(8.0),
                 margin: const EdgeInsets.all(8.0),
               ),
+              Container(
+                color: Colors.green,
+                width: 100,
+                height: 100,
+                padding: const EdgeInsets.all(8.0),
+                margin: const EdgeInsets.all(8.0),
+              ),
             ],
           ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Row in Column & Container in Row', style: TextStyle(fontSize: 24),),
+              Row(
+                children: [
+                  Container(
+                    child: Text(
+                      'Widget in Row',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    padding: const EdgeInsets.all(20.0),
+                    margin: const EdgeInsets.all(20.0),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Text('Container in Row'),
+                    color: Colors.red,
+                    width: 100,
+                    height: 200,
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                  ),
+                  Container(
+                    child: Text('Container'),
+                    color: Colors.green,
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                  ),
+                  Container(
+                    child: Text('Container'),
+                    color: Colors.orange,
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Container in Stack', style: TextStyle(fontSize: 24),),
+              Stack(
+                children: <Widget>[
+                  Container(
+                    child: Text('Container'),
+                    color: Colors.orange,
+                    width: 100,
+                    height: 100,
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                  ),
+                  Container(
+                    child: Text('Container'),
+                    color: Colors.red,
+                    width: 80,
+                    height: 80,
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                  ),
+                  Container(
+                    child: Text('Container'),
+                    color: Colors.yellow,
+                    width: 60,
+                    height: 60,
+                    padding: const EdgeInsets.all(8.0),
+                    margin: const EdgeInsets.all(8.0),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+          SingleChildScrollView(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Text('SingleChildScrollView', style: TextStyle(fontSize: 24),),
+                ListBody(
+                  children: items.map((i) => Text('$i')).toList(),
+                ),
+              ],
+            ),
+          ),
         ],
-      ), // 여러페이지를 좌우로 슬라이드하여 넘길 수 있도록 해 주는 위젯
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:survival_flutter/01_sample_app.dart';
 
-<<<<<<< HEAD
-
-void main() => runApp(MyAppMain());
-
-class MyAppMain extends StatelessWidget {
-  const MyAppMain({Key? key}) : super(key: key);
-=======
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
->>>>>>> dba704c081d9276fd051c5bfd44f690981f8dd5f
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-=======
       title: 'Flutter Study Note',
       theme: ThemeData(primarySwatch: Colors.blue),
->>>>>>> dba704c081d9276fd051c5bfd44f690981f8dd5f
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Study List'),
         ),
-<<<<<<< HEAD
         body: ListView(
           scrollDirection: Axis.vertical,
           children: <Widget>[
@@ -46,13 +30,6 @@ class MyApp extends StatelessWidget {
               },
             ),
           ],
-=======
-        body: Container(
-          child: Text(
-              'Hello World!!!!!',
-            style: TextStyle(fontSize: 40),
-          ),
->>>>>>> dba704c081d9276fd051c5bfd44f690981f8dd5f
         ),
       ),
     );
